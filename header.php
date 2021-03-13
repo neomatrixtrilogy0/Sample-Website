3d0b6eea9962a69c57646c030dc0e3c76fcd4143
neo <neo@neo>
2021-03-13 14:00:55 -0500

    Added a navbar header

---
+++ header.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+<div>myWebsite</div>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="index.php" class="navbar-brand">MyWebsite</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="lead.php">Lead Generation Services</a></li>
+                    <li><a href="demand.php">Demand Generation Services</a></li>
+                    <li><a href="emailmarket.php">Email Marketing Services</a></li>
+                    <li><a href="account.php">Account Profiling Services</a></li>
+                    <li><a href="appointment.php">Appointment Generation Services</a></li>
+                </ul>
+            </li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+    </div>
+</nav>

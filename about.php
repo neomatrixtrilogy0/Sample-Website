e00d6876f4b454b051db519b2977b44849f3d8a6
neo <neo@neo>
2021-03-13 22:28:52 -0500

    updated wording on about.php

--- about.php
+++ about.php
@@ -14,18 +14,13 @@
     <?php 
         include "header.php";
     ?>
-    <div class="container-fluid" id="content" style="background-image: url('images/office.jpg');">
+    <div class="container-fluid" id="content" >
         <h1 style="text-align:center; font-size: 72px;">About Us</h1>
         <p class="text1">
-            This might be the most distinctive website we’ve come across.
-
-            Anton & Irene is a design agency based in Manhattan.
-
-            Why are they a distinctive web presence?
-
-            Because their page takes parallax scrolling to the next level.
-
-            The snowy effects, bold colors, and quirky visuals create a truly captivating experience
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
+            laborum.
         </p>
     </div>
    

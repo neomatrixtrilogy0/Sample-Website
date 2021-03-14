670930ee3d9df0c9f56767e44cba97be965f2b90
neo <neo@neo>
2021-03-13 21:53:28 -0500

    Finished creating the homepage....still will come back to change the background as I am having issue with background-image

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website - Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styling.css">
+    
+</head>
+<body >
+    <?php
+        include "header.php"
+    ?>
+    <div class="container-fluid" id="content" >
+        <div id="txt">
+            <center><h2>We are best in B2B Services</h2>
+                <h2>Quality and Quantity amount of Data</h2>
+                <h2>Best Leads</h2>
+            </center>
+        </div>
+    </div>
+    <div id="foot">
+        <?php
+            include "footer.php"
+        ?>
+    </div>
+    
+</body>
+</html>
